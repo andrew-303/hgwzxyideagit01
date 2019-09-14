ed4e0e9e302a66cd6995d44617fc0a8c853ec055
andrew-303 <303247051@qq.com>
2019-09-14 23:18:46 +0800

    Third commit

--- src/com/biyl/git/Test01.java
+++ src/com/biyl/git/Test01.java
@@ -4,6 +4,5 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("这是第一行代码");
         System.out.println("这是第二行代码");
-        System.out.println("12345");
     }
 }
